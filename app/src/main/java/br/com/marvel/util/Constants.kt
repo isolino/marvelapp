415824a3243d997f3da8c76f175a569cc69ea9c5
isolino.ferreira <isolino.ferreira@meupag.com.br>
2019-10-21 13:47:15 -0300

    removendo timeout constant

--- app/src/main/java/br/com/marvel/util/Constants.kt
+++ app/src/main/java/br/com/marvel/util/Constants.kt
@@ -2,7 +2,6 @@ package br.com.marvel.util
 
 object Constants{
     const val BASE_URL = "https://gateway.marvel.com:443/v1/public/"
-    const val NETWORK_TIMEOUT = 10000L
 
     const val PUBLIC_KEY = "95f8d98006c25d01aadb76a0cbcb0075"
     const val PRIVATE_KEY = "55f08852affa4380fd5cf0ec3534f05068c132af"
